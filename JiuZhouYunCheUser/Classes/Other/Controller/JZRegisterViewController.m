d5c0f2f9ffaedda9fc8935046533ab2d312837dd
Eva-xiaoyan <1039397846@qq.com>
2017-12-08 18:15:37 +0800

    简单的业务实现，框架搭建

---
+++ JiuZhouYunCheUser/Classes/Other/Controller/JZRegisterViewController.m
@@ -0,0 +1,44 @@
+//
+//  JZRegisterViewController.m
+//  JiuZhouYunCheUser
+//
+//  Created by apple on 2017/12/7.
+//  Copyright © 2017年 曹晓燕. All rights reserved.
+//  注册页
+
+#import "JZRegisterViewController.h"
+
+@interface JZRegisterViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *pwdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPwdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *codeTextField;
+
+@end
+
+@implementation JZRegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+#pragma mark - 按钮点击
+//获取验证码
+- (IBAction)getCodeClick {
+}
+
+//注册
+- (IBAction)registerClick {
+}
+
+//登录按钮点击
+- (IBAction)loginClick {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
