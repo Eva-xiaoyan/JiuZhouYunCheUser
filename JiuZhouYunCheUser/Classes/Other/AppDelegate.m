6a2237dada6e62b6658d0fcf9f616bd63ad3e390
hengshi <hengshi@caffydemacbook-air.local>
2017-12-29 17:16:59 +0800

    改变

--- JiuZhouYunCheUser/Classes/Other/AppDelegate.m
+++ JiuZhouYunCheUser/Classes/Other/AppDelegate.m
@@ -28,18 +28,18 @@
     self.window = [[UIWindow alloc] init];
     self.window.frame = [UIScreen mainScreen].bounds;
     
-//    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-//    if ([userDefaults objectForKey:@"username"]) {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    if ([userDefaults objectForKey:@"username"]) {
         // 设置根控制器
-//        self.window.rootViewController = [[JZTabBarController alloc] init];
-//    }else if (![userDefaults boolForKey:@"notFirst"]){
-//        // 如果是第一次进入引导页
-//        self.window.rootViewController = [[JZGuidePageViewController alloc] init];
-//    }else{
-//        // 设置根控制器
+        self.window.rootViewController = [[JZTabBarController alloc] init];
+    }else if (![userDefaults boolForKey:@"notFirst"]){
+        // 如果是第一次进入引导页
+        self.window.rootViewController = [[JZGuidePageViewController alloc] init];
+    }else{
+        // 设置根控制器
         JZNavigationController *nav = [[JZNavigationController alloc]initWithRootViewController:[[JZRegisterViewController alloc] init]];
         self.window.rootViewController = nav;
-//    }
+    }
     
     // 显示窗口
     [self.window makeKeyAndVisible];
